56d0d0d61b928b2f8a5eafd838cad0f99a57c42a
Goldenstriker <ndparab240@gmail.com>
2019-12-20 17:40:26 +0530

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SearchformComponent } from './searchform/searchform.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, NavbarComponent, SearchformComponent ],
+  declarations: [ AppComponent, NavbarComponent, SearchformComponent, CarouselComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
